7e3d20d7ad724f0ec653ebc3ddd816b7091410bd
Sidhant Panda <sidhantpanda@gmail.com>
2014-03-20 23:53:47 +0530

    error catching left

--- sample.r
+++ sample.r
@@ -1,15 +1,3 @@
-#Source : http://www.mayin.org/ajayshah/KB/R/html/b2.html
-
-
-# Goal: To do sorting.
-#
-# The approach here needs to be explained. If `i' is a vector of
-# integers, then the data frame D[i,] picks up rows from D based
-# on the values found in `i'.
-#
-# The order() function makes an integer vector which is a correct
-# ordering for the purpose of sorting.
-
 D <- data.frame(x=c(1,2,3,1), y=c(7,19,2,2))
 D
 
