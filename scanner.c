503861212fa996cf8070d4e5ff3331b77cfaf914
Sidhant Panda <sidhantpanda@gmail.com>
2014-03-20 21:15:11 +0530

    need to add logic

--- scanner.c
+++ scanner.c
@@ -7,14 +7,21 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 int main(int argc,char *argv[])
 {
 	//Defining keywords
-	char *keys[19]={"if","else","repeat","while","function","for","in","next","break","TRUE","FALSE","NULL","Inf","NaN","NA","NA_integer_","NA_real_","NA_complex_","NA_character_"};
+	char *keys[21]={"if","else","repeat","while","function","for","in","next","break","TRUE","FALSE","NULL","Inf","NaN","NA","NA_integer_","NA_real_","NA_complex_","NA_character_","order","rev"};
 
 	//Defining data types
 	char *data_types[4]={"matrix","data.frame","list","factor"};
+
+	char *gouping[4]={
+		"(",")","{","}"
+	};
+
+	char *delimit={";"};
 
 	//Defining operators
 	char *operators[7][6]={
@@ -30,16 +37,28 @@ int main(int argc,char *argv[])
 	//Defining type of operator
 	char *type[30]={"TK_op_arithmetic","TK_op_relational","TK_op_logical","TK_op_modelf","TK_op_assignment","TK_op_indexing","TK_op_sequence"};
 
-	char ch;
 	int line_number=0;
+	size_t len=0;
+	ssize_t read;
+	char *line=NULL;
+	int flag=0;
+	char *token;
+	int i=0,j=0,k=0;
+
+	printf("Names: Raghav Dave, S*aksham Sahai, Sidhant Panda\n");
+	printf("Input: %s\n",argv[1]);
+	printf("Line\tType\tLexeme\n");
+	printf("----------------------\n");
 
 	FILE *source;
 	source=fopen(argv[1],"r");
 
-	/*
-	Logic Here
-	*/
-
+	while ((read = getline(&line, &len, source)) != -1)
+	{
+		
+	}
+	
+	// printf("%d\n",line_number );
   	fclose(source);
 	return 0;
 }
